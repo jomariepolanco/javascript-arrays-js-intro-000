6d2032077b75d17925cbeaefe0752d2145ec0014
versatile-utility-4919 <jomariepolanco@gmail.com>
2020-07-18 14:44:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -15,9 +15,10 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-
+  return [element, ...array]
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-
+  array.push(element)
+  return array
 }
