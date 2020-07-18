dae9f71e998f66071d7839398d5e3ba15f7c3ef7
versatile-utility-4919 <jomariepolanco@gmail.com>
2020-07-18 13:53:34 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,15 +4,3 @@ var chocolateBars = [
   'kitkat',
   'skittles'
 ]
-
-var addElementToBeginningOfArray = []
-function addElementToBeginningOfArray([1],'foo') {
-  addElementToBeginningOfArray.unshift
-  return addElementToBeginningOfArray
-}
-
-var destructivelyAddElementToBeginningOfArray = []
-function destructivelyAddElementToBeginningOfArray([1],'foo') {
-  destructivelyAddElementToBeginningOfArray = ['foo',...addElementToBeginningOfArray]
-  return destructivelyAddElementToBeginningOfArray
-}
