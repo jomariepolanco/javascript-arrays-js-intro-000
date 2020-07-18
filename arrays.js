4105c39e6133b04e42292686c70ea68966951653
versatile-utility-4919 <jomariepolanco@gmail.com>
2020-07-18 14:26:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -8,7 +8,7 @@ var chocolateBars = [
 var addElementToBeginningOfArray
 function addElementToBeginningOfArray([],n) {
   const array = ['foo']
-  addElementToBeginningOfArray === array.unshift(1)
+  array.unshift(1)
   return addElementToBeginningOfArray()
 }
 var destructivelyAddElementToBeginningOfArray
